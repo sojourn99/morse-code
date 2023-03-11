12c7402e52541b08f0a346c717ca841767a7cf13
aberg <alexafm.berg@gmail.com>
2023-03-11 16:44:44 +0100

    add unit tests

---
+++ test_morse.py
@@ -0,0 +1,11 @@
+from morse import convert_to_morse_code
+import pytest
+
+
+def test_morse_encode_correct_input():
+    assert convert_to_morse_code("sos") == "● ● ●   ■■■ ■■■ ■■■   ● ● ●"
+
+
+def test_morse_encode_incorrect_input():
+    with pytest.raises(KeyError):
+        convert_to_morse_code("@")
