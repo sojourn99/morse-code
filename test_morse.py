c47149abeecfd523c51315ff0097d4b6bc13265e
aberg <alexafm.berg@gmail.com>
2023-03-11 21:55:22 +0100

    add unit tests, assert exception messages

--- test_morse.py
+++ test_morse.py
@@ -4,17 +4,21 @@ import pytest
 
 def test_morse_encode_correct_input():
     assert convert_to_morse_code("sos") == "● ● ●   ■■■ ■■■ ■■■   ● ● ●"
+    assert convert_to_morse_code("sos sos") == "● ● ●   ■■■ ■■■ ■■■   ● ● ●       ● ● ●   ■■■ ■■■ ■■■   ● ● ●"
 
 
 def test_morse_encode_incorrect_input():
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as exc_info:
         convert_to_morse_code("@")
+    assert str(exc_info.value) == "'Cannot encode character: @'"
 
 
 def test_morse_decode_correct_input():
     assert convert_from_morse_code(". . .   - - -   . . .") == "sos"
+    assert convert_from_morse_code(". . .   - - -   . . .       . . .   - - -   . . .") == "sos sos"
 
 
 def test_morse_decode_incorrect_input():
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as exc_info:
         convert_from_morse_code("...")
+        assert str(exc_info.value) == "'Cannot decode morse code symbol: ...'"
