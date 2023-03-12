cd4c1327c4547fa2b5e6729be878174a860cdd4f
aberg <alexafm.berg@gmail.com>
2023-03-12 14:38:41 +0100

    add unit tests for input validation functions

--- test_morse.py
+++ test_morse.py
@@ -1,5 +1,21 @@
-from morse import convert_to_morse_code, convert_from_morse_code
+from morse import convert_to_morse_code, convert_from_morse_code, validate_morse_code, validate_text_input
 import pytest
+
+
+def test_validate_text_input_correct():
+    assert validate_text_input("SOS")
+
+
+def test_validate_text_input_incorrect():
+    assert not validate_text_input("SOS!")
+
+
+def test_validate_morse_input_correct():
+    assert validate_morse_code(". . .")
+
+
+def test_validate_morse_input_incorrect():
+    assert not validate_morse_code(". - @")
 
 
 def test_morse_encode_correct_input():
